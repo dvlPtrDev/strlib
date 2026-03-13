a748d7926ff9a5ba89f7e52c3b255176615f71ba
dvldev <davilapietro.sage@gmail.com>
2026-03-13 19:11:15 -0300

    Rename main.c to example.c and remove it from src

---
+++ src/example.c
@@ -0,0 +1,42 @@
+#include <strlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+void test_string_functions(void) {
+    String s1 = new_string(10);
+    s1.string_from(&s1, "Hello");
+    String s2 = new_string(20);
+    string_copy(s2.data, s1.data, 0);
+    string_concat(&s1, "!!!");
+    replace_char(s1.data, 'l', 'x');
+
+    assert(!string_compare(s1.data, s2.data));
+    assert(string_compare("Hexxo!!!", s1.data)); // ajusta se necessário
+
+    unregister_string(&s1, true);
+    unregister_string(&s2, true);
+}
+
+void test_is_numeric(void) {
+    String sn1 = new_string(10);
+    String sn2 = new_string(10);
+    sn1.string_from(&sn1, "12345");
+    sn2.string_from(&sn2, "12a45");
+    
+    assert(is_numeric(sn1.data));
+    assert(!is_numeric(sn2.data));
+    
+    unregister_string(&sn1, true);
+    unregister_string(&sn2, true);
+}
+
+int main(void) {
+    printf("=== Rodando testes ===\n");
+
+    test_string_functions();
+    test_is_numeric();
+
+    printf("Todos os testes passaram!\n");
+    return 0;
+}
